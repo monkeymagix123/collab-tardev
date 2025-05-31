9043b0ad9e4cae323c48e6518b0e96d49614acf6
sus <iamsuchabozo12345@gmail.com>
2025-05-31 14:43:07 -0700

    wut

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,14 @@
 
 <script setup lang="ts">
     import game from './setup';
+    import Button from './components/Button.vue';
 </script>
 
 <style src="./assets/tailwind.css"></style>
+<!-- <style scoped>
+    @reference "./assets/tailwind.css";
+
+    button {
+        @apply bg-yellow-400 hover:bg-yellow-500 text-purple-900 font-bold py-4 px-8 rounded-full shadow-lg transform transition-all duration-200 ease-in-out active:scale-95 focus:outline-none focus:ring-4 focus:ring-yellow-300 focus:ring-opacity-75 text-2xl
+    }
+</style> -->
