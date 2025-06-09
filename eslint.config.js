df5375734c6765409f887d1784e04054a5cd7c07
sus <iamsuchabozo12345@gmail.com>
2025-06-09 12:31:21 -0700

    more format some stuff

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import vue from 'eslint-plugin-vue';
+
+export default [
+	{
+		files: ['*.js', '*.ts', '*.vue'],
+		languageOptions: {
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+			globals: {
+				window: 'readonly',
+				document: 'readonly',
+				// add any globals you need here
+			},
+		},
+		plugins: {
+			vue,
+		},
+		rules: {
+			'no-console': 'warn',
+			// Vue rules
+			'vue/html-indent': ['error', 2],
+			'vue/max-attributes-per-line': [
+				'error',
+				{
+					singleline: 3,
+					multiline: {
+						max: 1,
+						allowFirstLine: false,
+					},
+				},
+			],
+		},
+		extends: [
+			'eslint:recommended',
+			'plugin:vue/vue3-recommended',
+			'plugin:prettier/recommended',
+		],
+		settings: {},
+	},
+];
